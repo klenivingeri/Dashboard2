f7b59bb92708e4dbb36df18cdbf44dc2badf7ccf
Erick <klenivingeri@gmail.com>
2021-05-21 12:22:33 -0300

    testando Footer

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import Header from '../components/Header'
-import {Footer} from '../components/Footer'
+
 import { theme } from '../styles/theme'
 
 function MyApp({ Component, pageProps }) {
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }) {
     <ChakraProvider theme={theme}>
       <Header />
     <Component {...pageProps} />
-      <Footer />
+
     </ChakraProvider>
     )
   
